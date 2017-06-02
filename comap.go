bd8aadcb4ffd454770276aaad20ca6fb691af41c
Aaron <aircitybruno@gmail.com>
2017-06-02 22:23:53 +0800

    test

--- comap.go
+++ comap.go
@@ -2,10 +2,6 @@ package comap
 
 import "sync"
 
-var SIZE int = 32
-
-type value interface {}
-
 // The definition to thread-safe concurrent map.
 type Comap struct {
 	comap	map[string]interface{}
@@ -13,7 +9,6 @@ type Comap struct {
 }
 
 // TODO: figure out the reason for []*Comap
-type ConcurrentMap []*Comap
 
 func (comap Comap) GetValue(key string) interface{} {
 	comap.RLock()
@@ -27,11 +22,7 @@ func (comap Comap) SetValue(key string, value interface{}) {
 	comap.comap[key] = value
 }
 
-func New() ConcurrentMap {
+func New() *Comap {
 	// TODO:figure out the reason for adding size in make()
-	m := make(ConcurrentMap, SIZE)
-	for i := 0; i < SIZE; i++ {
-		m[i] = &Comap{comap:make(map[string]interface{})}
-	}
-	return m
+	return &Comap{comap:make(map[string]interface{})}
 }
