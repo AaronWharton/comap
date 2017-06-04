7572963d4ddc1f67e1e9d5c6701a5923a4ec3f27
Aaron <aircitybruno@gmail.com>
2017-06-04 13:17:12 +0800

    add  instruction

--- comap.go
+++ comap.go
@@ -4,41 +4,41 @@ import (
 	"sync"
 )
 
+// Specify the number of the elements when CoMap is allocated.
 var COUNT = 32
 
-type ConcurrentMap []*Comap
+type CoMap []*ConcurrentMap
 
 // The definition to thread-safe concurrent map.
-type Comap struct {
-	comap map[string]interface{}
+type ConcurrentMap struct {
+	concurrentMap map[string]interface{}
 	sync.RWMutex
 }
 
-// TODO: figure out the reason for []*Comap
-
-func (m ConcurrentMap) Get(key string) (interface{}, bool) {
-	// Get shard
-	shard := m.GetShard(key)
-	shard.RLock()
-	// Get item from shard.
-	val, ok := shard.comap[key]
-	shard.RUnlock()
+func (m CoMap) Get(key string) (interface{}, bool) {
+	// Get elem
+	elem := m.GetShard(key)
+	elem.RLock()
+	// Get ConcurrentMap from elem.
+	val, ok := elem.concurrentMap[key]
+	elem.RUnlock()
 	return val, ok
 }
 
-func (m ConcurrentMap) Set(key string, value interface{}) {
-	// Get map shard.
-	shard := m.GetShard(key)
-	shard.Lock()
-	shard.comap[key] = value
-	shard.Unlock()
+func (m CoMap) Set(key string, value interface{}) {
+	// Get map elem.
+	elem := m.GetShard(key)
+	elem.Lock()
+	elem.concurrentMap[key] = value
+	elem.Unlock()
 }
 
-func (m ConcurrentMap) GetShard(key string) *Comap {
-	return m[uint(fnv32(key))%uint(COUNT)]
+func (m CoMap) GetShard(key string) *ConcurrentMap {
+	return m[uint(hash(key))%uint(COUNT)]
 }
 
-func fnv32(key string) uint32 {
+// TODO: more function ...
+func hash(key string) uint32 {
 	hash := uint32(2166136261)
 	const prime32 = uint32(16777619)
 	for i := 0; i < len(key); i++ {
@@ -48,11 +48,10 @@ func fnv32(key string) uint32 {
 	return hash
 }
 
-func New() ConcurrentMap {
-	// TODO:figure out the reason for adding size in make()
-	m := make(ConcurrentMap, COUNT)
+func New() CoMap {
+	m := make(CoMap, COUNT)
 	for i := 0; i < COUNT; i++ {
-		m[i] = &Comap{comap: make(map[string]interface{})}
+		m[i] = &ConcurrentMap{concurrentMap: make(map[string]interface{})}
 	}
 	return m
 }
