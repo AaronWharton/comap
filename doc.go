09932a56cbb87fa72ff5c1647f3568df4ee6bcac
Aaron <aircitybruno@gmail.com>
2017-07-26 09:32:52 +0800

    add doc

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 // Package comap is a simple implementation of concurrent map.
+// Attention:
+// 			This project is not a practical use project,
+// 			it is only for golang internal implementation learning.
+// 			If you find any problems, you can make a pull request or issue.
 //
 // More information see https://github.com/AaronWharton/comap.
 package comap
